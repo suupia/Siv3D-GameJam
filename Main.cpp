043a2e5f09d0c65369294fb9b789348f13371fc8
suupia <morita777k@icloud.com>
2023-10-23 17:58:30 +0900

    Change Main() to use MessageWindow class

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,6 @@
 ﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.11
+# include "MessageWindow.h"
+# include "RectFUtility.h"
 
 void set_up_window()
 {
@@ -15,48 +17,13 @@ void set_up_window()
 	Scene::SetBackground(ColorF{ 139/ 255.0f,69/ 255.0f,19/ 255.0f});
 }
 
-RectF calc_relative_rect(double offset_x, double offset_y, double width, double height)
-{
-	return RectF{ offset_x * Scene::Width(), offset_y * Scene::Height(), width * Scene::Width(), height * Scene::Height() };
-}
-
-Vec2 calc_center_pos(const RectF& rect)
-{
-	return Vec2{ rect.x + rect.w / 2.0, rect.y + rect.h / 2.0 };
-}
-
-// Transform coordinates to be centered horizontally
-RectF to_horizontal_center(const RectF& rect)
-{
-	const auto h_margin = Scene::Width() - rect.w;
-	return RectF{ h_margin /2.0 , rect.y, rect.w, rect.h };
-}
-
-
-void open_message_window(const Font& font , const String& name, const String& message)
-{
-	// back ground box
-	const auto back_ground_rect = calc_relative_rect(0, 3.0/5.0, 1, 2.0/5.0).draw(Palette::Lightslategray);
-
-	// message box
-	const auto message_rect= to_horizontal_center( RectF{back_ground_rect.x, back_ground_rect.y, back_ground_rect.w * 0.8, back_ground_rect.h} );
-	(void)message_rect.draw(Palette::Lightblue);
-	font(message).drawAt(calc_center_pos(message_rect), Palette::Black);
-
-	// name box
-	const auto rect = calc_relative_rect(1.0/30.0,21.0/40.0,1.0/4.0,1.0/8.0);
-	(void)rect.draw(Palette::Teal);
-	font(name).drawAt(calc_center_pos(rect), Palette::White);
-
-
-
-}
-
 void Main()
 {
 	const Font font{ FontMethod::MSDF, 48 };
 
 	set_up_window();
+
+	MessageWindow messageWindow;
 
 
 	String name = U"Temp Name";
@@ -65,7 +32,7 @@ void Main()
 
 	while (System::Update())
 	{
-		open_message_window(font, name, message);
+		messageWindow.open_message_window(font, name, message);
 	}
 }
 
