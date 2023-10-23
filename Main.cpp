6e6f58a32545b05cf430e15279304d28a3965076
suupia <morita777k@icloud.com>
2023-10-23 16:25:08 +0900

    Add temp name to name box

--- Main.cpp
+++ Main.cpp
@@ -15,28 +15,36 @@ void set_up_window()
 	Scene::SetBackground(ColorF{ 139/ 255.0f,69/ 255.0f,19/ 255.0f});
 }
 
-RectF get_relative_rect(double offset_x, double offset_y, double width, double height)
+RectF calc_relative_rect(double offset_x, double offset_y, double width, double height)
 {
 	return RectF{ offset_x * Scene::Width(), offset_y * Scene::Height(), width * Scene::Width(), height * Scene::Height() };
 }
 
-void open_message_window()
+Vec2 calc_center_pos(const RectF& rect)
+{
+	return Vec2{ rect.x + rect.w / 2.0, rect.y + rect.h / 2.0 };
+}
+
+void open_message_window(const Font& font)
 {
 	// message box
-	get_relative_rect(0, 3.0/5.0, 1, 2.0/5.0).draw(Palette::Lightslategray);
+	(void)calc_relative_rect(0, 3.0/5.0, 1, 2.0/5.0).draw(Palette::Lightslategray);
 
 	// name box
-	get_relative_rect(1.0/30.0,21.0/40.0,1.0/4.0,1.0/8.0).draw(Palette::Teal);
+	const auto rect = calc_relative_rect(1.0/30.0,21.0/40.0,1.0/4.0,1.0/8.0);
+	(void)rect.draw(Palette::Teal);
+	font(U"Name").drawAt(calc_center_pos(rect), Palette::White);
 
 }
 
 void Main()
 {
+	const Font font{ FontMethod::MSDF, 48 };
 
 	set_up_window();
 	while (System::Update())
 	{
-		open_message_window();
+		open_message_window(font);
 	}
 }
 
