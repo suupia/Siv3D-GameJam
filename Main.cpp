4088a3762de07607b1b7748ecbc1ac784cc5c8d7
suupia <morita777k@icloud.com>
2023-10-23 15:23:28 +0900

    Add get_relative_rect()

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 ﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.11
 
-void SetUpWindow()
+void set_up_window()
 {
 	constexpr  int windowWidth = 1920;
 	constexpr  int windowHeight = 1080;
@@ -11,16 +11,32 @@ void SetUpWindow()
 	Window::Resize(windowWidth, windowHeight);
 	Window::SetStyle(WindowStyle::Sizable);
 
+	// Set Background Color
 	Scene::SetBackground(ColorF{ 139/ 255.0f,69/ 255.0f,19/ 255.0f});
+}
+
+RectF get_relative_rect(double offset_x, double offset_y, double width, double height)
+{
+	return RectF{ offset_x * Scene::Width(), offset_y * Scene::Height(), width * Scene::Width(), height * Scene::Height() };
+}
+
+void open_message_window()
+{
+	// message box
+	get_relative_rect(0, 3.0/5.0, 1, 2.0/5.0).draw(Palette::Lightslategray);
+
+	// name box
+	get_relative_rect(1.0/10.0,4.0/10.0,1.0/2.0,2.0/5.0).draw(Palette::Teal);
+
 }
 
 void Main()
 {
 
-	SetUpWindow();
+	set_up_window();
 	while (System::Update())
 	{
-
+		open_message_window();
 	}
 }
 
