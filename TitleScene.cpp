fc0a7b1dcc13d79cb9e547daee2fd4eab2c6926b
suupia <morita777k@icloud.com>
2023-10-31 11:28:34 +0900

    Add temp title to TitleScene

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,7 +1,9 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "TitleScene.h"
 
-TitleScene::TitleScene(const InitData& init): IScene(init)
+#include "RectFUtility.h"
+
+TitleScene::TitleScene(const InitData& init): IScene(init), gm_(GameManager()), font_{ FontMethod::MSDF, 48 }
 {
 	// back ground
 	TextureAsset::Register(U"Windmill", U"example/windmill.png");
@@ -12,4 +14,8 @@ void TitleScene::update(){
 
 void TitleScene::draw() const {
 	(void)TextureAsset(U"PhotoStudio").resized(Scene::Width(),Scene::Height()).draw(0, 0);
+
+	const auto message_rect= RectFUtility::to_horizontal_center( RectF{50,100,Scene::Width(),Scene::Height()} );
+	font_(U"写真館<<黄泉の前>>").drawAt(Scene::Width() / 2, Scene::Height() / 2, Palette::Black);
+
 }
