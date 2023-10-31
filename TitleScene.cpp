5e266806054bf149adee604f5b83a3bc8d94063e
suupia <morita777k@icloud.com>
2023-10-31 11:43:09 +0900

    Add changeScene() to TitleScene

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,15 +1,39 @@
 ﻿#include "stdafx.h"
 #include "TitleScene.h"
-
 #include "RectFUtility.h"
 
-TitleScene::TitleScene(const InitData& init): IScene(init), gm_(GameManager()), font_{ FontMethod::MSDF, 48 }
+namespace
+{
+	bool Button(const Rect& rect, const Font& font, const String& text, bool enabled)
+	{
+		if (enabled && rect.mouseOver())
+		{
+			Cursor::RequestStyle(CursorStyle::Hand);
+		}
+
+		if (enabled)
+		{
+			rect.draw(ColorF{ 0.3, 0.7, 1.0 });
+			font(text).drawAt(40, (rect.x + rect.w / 2), (rect.y + rect.h / 2));
+		}
+		else
+		{
+			rect.draw(ColorF{ 0.5 });
+			font(text).drawAt(40, (rect.x + rect.w / 2), (rect.y + rect.h / 2), ColorF{ 0.7 });
+		}
+
+		return (enabled && rect.leftClicked());
+	}
+}
+
+TitleScene::TitleScene(const InitData& init): IScene(init),gm_(GameManager()),font_{FontMethod::MSDF, 48}
 {
 	// back ground
 	TextureAsset::Register(U"Windmill", U"example/windmill.png");
 }
 
 void TitleScene::update(){
+	if(KeyA.down()) changeScene(SceneState::Prologue, 2.0s);
 }
 
 void TitleScene::draw() const {
