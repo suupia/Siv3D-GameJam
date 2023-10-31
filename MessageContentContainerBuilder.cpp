089862c479e6e067788080a648341312c2b3c4ab
suupia <morita777k@icloud.com>
2023-10-31 17:03:15 +0900

    Add MessageContentContainerBuilder.cpp

---
+++ MessageContentContainerBuilder.cpp
@@ -0,0 +1,24 @@
+﻿#include "stdafx.h"
+#include "MessageContentContainerBuilder.h"
+
+#include "MessageContentPictureAttacher.h"
+#include "MessageReader.h"
+
+
+MessageContentContainer MessageContentContainerBuilder::build_message_content_container(GameManager& gm, const Font& font) const
+{
+	MessageContentContainer message_content_container(gm, font);
+
+	const auto message_structs = MessageReader(path_).readMessageAll();
+	const auto message_content_structs = MessageContentPictureAttacher().create_message_content_struct(message_structs);
+
+	Array<MessageContent> message_contents;
+	for (auto content_struct : message_content_structs)
+	{
+		message_contents.push_back(MessageContent(font, content_struct));
+	}
+	message_content_container.add_message_contents(message_contents);
+
+	return message_content_container;
+}
+
---
+++ MessageContentContainerBuilder.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "MessageContentContainer.h"
+
+class MessageContentContainerBuilder
+{
+	String path_;
+public:
+	MessageContentContainerBuilder(String path);
+   MessageContentContainer build_message_content_container(GameManager& gm, const Font& font)const;
+};
