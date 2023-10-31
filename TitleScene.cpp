14a55688c27e645137c4aad7a53ae8dc549a569b
suupia <morita777k@icloud.com>
2023-10-31 15:09:40 +0900

    Remove anonymous Button()

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,32 +1,13 @@
 ﻿#include "stdafx.h"
 #include "TitleScene.h"
-#include "RectFUtility.h"
 
-namespace
-{
-	bool Button(const RectF& rect, const Font& font, const String& text, bool enabled)
-	{
-		if (enabled && rect.mouseOver())
-		{
-			Cursor::RequestStyle(CursorStyle::Hand);
-		}
-
-		if (enabled)
-		{
-			rect.draw(ColorF{ 0.3, 0.7, 1.0 });
-			font(text).drawAt(40, (rect.x + rect.w / 2), (rect.y + rect.h / 2));
-		}
-		else
-		{
-			rect.draw(ColorF{ 0.5 });
-			font(text).drawAt(40, (rect.x + rect.w / 2), (rect.y + rect.h / 2), ColorF{ 0.7 });
-		}
-
-		return (enabled && rect.leftClicked());
-	}
-}
-
-TitleScene::TitleScene(const InitData& init): IScene(init),gm_(GameManager()),font_{FontMethod::MSDF, 48}, start_button_(RectF{Scene::Width() / 2, Scene::Height() / 2 + 100, 200, 100}, font_, U"はじめる")
+TitleScene::TitleScene(const InitData& init):
+	IScene(init),
+	gm_(GameManager()),
+	font_{FontMethod::MSDF, 48},
+	start_button_(RectF{
+		              Scene::Width() / 2, Scene::Height() / 2 + 100, 200, 100
+	              }, font_, U"はじめる")
 {
 }
 
