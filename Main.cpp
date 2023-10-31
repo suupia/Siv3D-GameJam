1665b704d66fd49509a3ac9572a03f97b9c457b1
suupia <morita777k@icloud.com>
2023-10-31 09:29:30 +0900

    set_up_window() to anonymous

--- Main.cpp
+++ Main.cpp
@@ -5,21 +5,24 @@
 #include "MessageWindowContainer.h"
 # include "RectFUtility.h"
 
-void set_up_window()
+namespace
 {
-	constexpr  int windowWidth = 1920;
-	constexpr  int windowHeight = 1080;
-	if(windowWidth * 9 != windowHeight * 16)
+	void set_up_window()
 	{
-		throw Error(U"You should set the resolution to 16:9");
+		constexpr  int windowWidth = 1920;
+		constexpr  int windowHeight = 1080;
+		if(windowWidth * 9 != windowHeight * 16)
+		{
+			throw Error(U"You should set the resolution to 16:9");
+		}
+		Window::Resize(windowWidth, windowHeight);
+		Window::SetStyle(WindowStyle::Sizable);
+
+
+
+		// Set Background Color
+		Scene::SetBackground(ColorF{ 139/ 255.0f,69/ 255.0f,19/ 255.0f});
 	}
-	Window::Resize(windowWidth, windowHeight);
-	Window::SetStyle(WindowStyle::Sizable);
-
-
-
-	// Set Background Color
-	Scene::SetBackground(ColorF{ 139/ 255.0f,69/ 255.0f,19/ 255.0f});
 }
 
 void Main()
@@ -37,18 +40,18 @@ void Main()
 	TextureAsset::Register(U"PhotoStudio", U"images/photo_studio.png") ;
 	TextureAsset::Register(U"Me", U"images/siv3d-kun.png");
 
-	// MessageWindowContainer message_window_container(gm, font);
-	// auto message_structs = message_reader.readMessageAll();
-	//
-	// auto message_content_structs = message_content_picture_attacher.create_message_content_struct(message_structs);
-	//
-	// Array<MessageContent> message_windows_;
-	// for(auto content_struct : message_content_structs)
-	// {
-	// 	message_windows_.push_back(MessageContent(font,content_struct));
-	// }
-	//
-	// message_window_container.add_message_contents(message_windows_);
+	MessageWindowContainer message_window_container(gm, font);
+	auto message_structs = message_reader.readMessageAll();
+
+	auto message_content_structs = message_content_picture_attacher.create_message_content_struct(message_structs);
+
+	Array<MessageContent> message_windows_;
+	for(auto content_struct : message_content_structs)
+	{
+		message_windows_.push_back(MessageContent(font,content_struct));
+	}
+
+	message_window_container.add_message_contents(message_windows_);
 
 
 	while (System::Update())
@@ -56,18 +59,10 @@ void Main()
 		// draw background
 		(void)TextureAsset(U"PhotoStudio").resized(Scene::Width(),Scene::Height()).draw(0, 0);
 
-		// message_window_container.update_logic();
-		// message_window_container.update_render();
-		//
-		// if(KeySpace.down()) message_window_container.go_to_next_message();
+		message_window_container.update_logic();
+		message_window_container.update_render();
 
-
-		if(Key0.down())
-		{
-			auto readOne = message_reader.readMessageOne();
-			Print <<  readOne.name;
-			Print << readOne.messages;
-		}
+		if(KeySpace.down()) message_window_container.go_to_next_message();
 	}
 }
 
