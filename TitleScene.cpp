02c9aed29e927f59005686dfd16fd8c7451bb650
suupia <morita777k@icloud.com>
2023-10-31 14:13:41 +0900

    Add tmp anonymous Button()

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -4,7 +4,7 @@
 
 namespace
 {
-	bool Button(const Rect& rect, const Font& font, const String& text, bool enabled)
+	bool Button(const RectF& rect, const Font& font, const String& text, bool enabled)
 	{
 		if (enabled && rect.mouseOver())
 		{
@@ -28,18 +28,23 @@ namespace
 
 TitleScene::TitleScene(const InitData& init): IScene(init),gm_(GameManager()),font_{FontMethod::MSDF, 48}
 {
-	// back ground
-	TextureAsset::Register(U"Windmill", U"example/windmill.png");
 }
 
 void TitleScene::update(){
 	if(KeyA.down()) changeScene(SceneState::Prologue, 2.0s);
+
+
 }
 
 void TitleScene::draw() const {
 	(void)TextureAsset(U"PhotoStudio").resized(Scene::Width(),Scene::Height()).draw(0, 0);
 
-	const auto message_rect= RectFUtility::to_horizontal_center( RectF{50,100,Scene::Width(),Scene::Height()} );
 	font_(U"写真館<<黄泉の前>>").drawAt(Scene::Width() / 2, Scene::Height() / 2, Palette::Black);
+
+	if(	Button(RectF{Scene::Width() / 2, Scene::Height() / 2 + 100, 200, 100}, font_, U"はじめる", true))
+	{
+		Print << U"はじめる";
+	}
+
 
 }
