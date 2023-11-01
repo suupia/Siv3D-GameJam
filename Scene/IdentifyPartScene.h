1149611940fa0172ee598c14a0b21066d0267426
suupia <morita777k@icloud.com>
2023-11-01 17:44:15 +0900

    Add IdentifyPartScene.cpp

---
+++ Scene/IdentifyPartScene.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "../GameManager/GameManager.h"
+#include "../Button/Button.h"
+#include "SceneSetting.h"
+
+class IdentifyPartScene : public SceneManager<SceneState, SceneData>::Scene
+{
+private:
+	GameManager gm_;
+	Font font_;
+	Array<Button> buttons_;
+public:
+	IdentifyPartScene(const InitData& init);
+
+	void update() override;
+
+	void draw() const override;
+
+};
