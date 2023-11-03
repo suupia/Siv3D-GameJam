544143eec5bdb4b8afaa096060699e0dd2b6d0cb
suupia <morita777k@icloud.com>
2023-11-03 16:34:39 +0900

    Remove redundent comment

--- Scene/IdentifyPartScene.cpp
+++ Scene/IdentifyPartScene.cpp
@@ -40,36 +40,22 @@ IdentifyPartScene::IdentifyPartScene(const InitData& init):
 	gm_(GameManager()),
 	font_{FontMethod::MSDF, 48}
 {
+	constexpr int photo_number = 6;
+
 	TextureAsset::Register(U"Book", U"images/book_tmp.png") ;
 	TextureAsset::Register(U"StickyNote", U"images/sticky_note_tmp.png") ;
+	for(int i = 0; i< photo_number; i++)
+	{
+		const auto path = U"images/identify_photo_tmp_{}.png"_fmt(i);
+		TextureAsset::Register(U"IdentifyPhoto{}"_fmt(i), path) ;
+	}
 
 	// ratio
 	double x = 0.03;
 	double y = 0.03;
 	double w = 0.43;
 	double h = 0.31;
-	// const auto button0 = Button( RectFUtility::calc_relative_rect(x,y,w,h), font_, U"0");
-	// const auto button1 = Button( RectFUtility::calc_relative_rect(x+w,y+h,w,h), font_, U"1");
-	// const auto button2 = Button( RectFUtility::calc_relative_rect(x,y+2*h,w,h), font_, U"2");
-	// const auto button3 = Button( RectFUtility::calc_relative_rect(x+2*w,y,w,h), font_, U"3");
-	// const auto button4 = Button( RectFUtility::calc_relative_rect(x+3*w,y+h,w,h), font_, U"4");
-	// const auto button5 = Button( RectFUtility::calc_relative_rect(x+2*w,y+2*h,w,h), font_, U"5");
-	// const auto sticky_note0 = RectFUtility::calc_relative_rect(x+w,y,w,h);
-	// const auto sticky_note1 = RectFUtility::calc_relative_rect(x+2*w,y+h,w,h);
-	// const auto sticky_note2 = RectFUtility::calc_relative_rect(x+w,y+2*h,w,h);
-	// const auto sticky_note3 = RectFUtility::calc_relative_rect(x,y+h,w,h);
-	// const auto sticky_note4 = RectFUtility::calc_relative_rect(x+3*w,y,w,h);
-	// const auto sticky_note5 = RectFUtility::calc_relative_rect(x+2*w,y+2*h,w,h);
-	//
-	//
-	// identify_photo_data_.push_back({button0, sticky_note0, false});
-	// identify_photo_data_.push_back({button1, sticky_note1, false});
-	// identify_photo_data_.push_back({button2, sticky_note2, false});
-	// identify_photo_data_.push_back({button3, sticky_note3, false});
-	// identify_photo_data_.push_back({button4, sticky_note4, false});
-	// identify_photo_data_.push_back({button5, sticky_note5, false});
 
-	constexpr int photo_number = 6;
 	for(int i = 0; i < photo_number; i++)
 	{
 		identify_photo_data_.push_back( create_identify_photo_data(i, x, y, w, h, font_));
