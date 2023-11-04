c0ba40ad63ef672fbbeb1c25336eddc009518622
suupia <morita777k@icloud.com>
2023-11-04 22:19:41 +0900

    Add title.png

--- Scene/TitleScene.cpp
+++ Scene/TitleScene.cpp
@@ -8,7 +8,7 @@ TitleScene::TitleScene(const InitData& init):
 	font_{FontMethod::MSDF, 48},
 	start_button_( RectFUtility::to_horizontal_center( RectFUtility::calc_relative_rect(0.5, 0.7, 0.1,0.08)), font_, U"はじめる")
 {
-	TextureAsset::Register(U"PhotoStudio", U"images/back_photoStudio.png") ;
+	TextureAsset::Register(U"Title", U"images/title.png") ;
 }
 
 void TitleScene::update(){
@@ -24,9 +24,8 @@ void TitleScene::update(){
 }
 
 void TitleScene::draw() const {
-	(void)TextureAsset(U"PhotoStudio").resized(Scene::Width(),Scene::Height()).draw(0, 0);
+	(void)TextureAsset(U"Title").resized(Scene::Width(),Scene::Height()).draw(0, 0);
 
-	font_(U"写真館<<黄泉の前>>").drawAt(Scene::Width() / 2, Scene::Height() / 2, Palette::Black);
 
 	start_button_.draw();
 }
