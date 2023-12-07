190cc38e25061ef5a2469aba7648cbc1262616f2
suupia <morita777k@icloud.com>
2023-12-07 11:00:04 +0900

    add is_show_texture_ flag to MessageContent

--- GameObject/MessageContent.h
+++ GameObject/MessageContent.h
@@ -9,24 +9,6 @@ struct MessageContentStruct {
 
 class MessageContent : public GameObject
 {
-private:
-	const Font& font_;
-
-	const String name_;
-	const String messages_;
-	const TextureAsset standing_picture_;
-
-	int message_char_index_ = 0;
-	String current_message_;
-	float interval_second_ = 0.07f;
-	double timer = 0.0f;
-
-	bool is_waiting_for_input_ = false;
-	bool is_showing_all_message_ = false;
-
-	void show_message_window(const String& name, const String& message) const;
-
-
 public:
 	MessageContent(const Font& font,
 		const MessageContentStruct& message_window_struct
@@ -46,4 +28,27 @@ public:
 	void show_all_message();
 	[[nodiscard]]
 	bool is_show_all_message() const { return is_showing_all_message_; }  // 実装ファイルに書いた方がよい？
+
+	void dont_show_texture() { is_show_texture_ = false; }
+
+private:
+	const Font& font_;
+	const String name_;
+	const String messages_;
+	const TextureAsset standing_picture_;
+
+	int message_char_index_ = 0;
+	String current_message_;
+	float interval_second_ = 0.07f;
+	double timer = 0.0f;
+
+	bool is_waiting_for_input_ = false;
+	bool is_showing_all_message_ = false;
+
+	// show_message_window flag
+	bool is_show_texture_ = true;
+	void show_message_window(const String& name, const String& message) const;
+
+
+
 };
